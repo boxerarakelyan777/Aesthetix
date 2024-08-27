6f5f9c6574e1d724b48824b7da122f772f3ad191
Rudik Arakelyan <rudik.arakelyan777@gmail.com>
2024-08-27 15:24:06 -0700

    Vercel Bug Fixes

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import React from "react";
 import HeroSection from "../../components/Hero";
-import Team from "../../components/Team";
+
 import ContactForm from "../../components/Contact";
-import SocialProof from "../../components/SocialProof"; 
+
 import Features from "../../components/Features";
 import Pricing from "../../components/Pricing";
-import ValuePropositions from "../../components/ValuePropositions";
+
 export default function Home() {
   return (
     <>
